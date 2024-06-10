26801178732a0bbc0ce765cedf6f71ae28b180d3
gustavopalaciosc <gpalaciosc@uc.cl>
2024-06-10 00:10:35 -0400

    fix(src): minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    const fetchArticle = async () => {
+
+      try {
+        const url = 'http://127.0.0.1:8000/article/?url=https://www.df.cl/economia-y-politica/macro/las-senales-economicas-en-mayo-se-modera-el-interes-por-el-consumo';
+        const response = await axios.get(url);
+        console.log(response.data['title']);
+        console.log(response.data['subtitle']);
+        console.log(response.data['body']);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchArticle();
+  }, []);
 
   return (
     <div className='main-content'>
