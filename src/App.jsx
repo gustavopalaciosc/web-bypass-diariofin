9068021bb6cfe7f6d720eae519f62884d3765da8
gustavopalaciosc <gpalaciosc@uc.cl>
2024-06-14 18:21:33 -0400

    feat: added netlify.toml and change in url request

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const fetchArticle = async (articleUrl) => {
     setLoading(true);
     try {
-      const response = await axios.get(`http://127.0.0.1:8000/article/?url=${articleUrl}`);
+      const response = await axios.get(`https://backend-web-diariofin.onrender.com/article/?url=${articleUrl}`);
       if (response.data['status_code'] == 200 ) {
       setTitle(response.data['title']);
       setSubtitle(response.data['subtitle']);
